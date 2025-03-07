ac1015ef3aa76375b3da1d7a7770a49566388212
augustusHarts <augustus.harts@gmail.com>
2025-03-07 10:08:23 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Ensure Next.js exports static files
   basePath: "/huzi", // Important: GitHub Pages serves from repo name
-  assetPrefix: "/huzi/",
+  assetPrefix: "/huzi/"
 };
 
 export default nextConfig;
