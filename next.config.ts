beb7e5642f6fbfdd8d6f453e117bee6e9a60095e
augustusHarts <augustus.harts@gmail.com>
2025-03-07 10:28:59 +0530

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Ensure Next.js exports static files
-  basePath: "/huzi", // Important: GitHub Pages serves from repo name
-  assetPrefix: "/huzi/"
+  basePath: "/portfolio-huzi", // Important: GitHub Pages serves from repo name
+  atrailingSlash: true,
 };
 
 export default nextConfig;
